4e3de44113501ab381c6db04c736bfb663f3dfa7
Sahil <sahilsharma2000dtc@gmail.com>
2024-11-17 20:56:26 +0530

    feat: models for the application and auth controller

---
+++ models/thumbnail.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const thumbnailSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    videoName: {
+        type: String,
+        required: true,
+    },
+    version: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    paid: {
+        type: String,
+        default: false
+    },
+});
+
+export const Thumbnail = mongoose.model("Thumbnail", thumbnailSchema);
